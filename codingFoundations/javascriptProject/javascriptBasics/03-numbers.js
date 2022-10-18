f48b8d586fa8b0fb71fba9e40d3e45918b08c9fa
Celio Arias <ariascelio@gmail.com>
2022-10-17 20:01:55 -0400

    operator

---
+++ codingFoundations/javascriptProject/javascriptBasics/03-numbers.js
@@ -0,0 +1,44 @@
+// Numbers in JavaScript
+0
+1
+0.99
+-42
+NaN
+
+// Storing numbers in Variables
+let theMeaningOfLife = 42;
+const indianaIncomeTaxRate = 0.0323;
+let powerLevel = 9001;
+let southPoleTempF = -44;
+let spaceStationCrewCount = 7;
+const imNotANumber = NaN;
+
+// Arithmetic Operators
+1 + 1;  // Addition
+
+12 - 10;  // Subtraction
+
+125 * 1.07;  // Multiplication
+
+125 / 4;  // Division
+
+10 ** 2;  // Exponentiation
+
+15 % 2;  // Modulus
+
+// ==== Mini-Challenge ====
+let billAmount = 132.96;
+let totalPeople = 5;
+const salesTax = 0.07;
+
+// Calculate the amount of tax on the bill
+let taxAmount = billAmount * salesTax
+
+// Calculate the grand total
+let grandTotal = billAmount + taxAmount;
+
+// Calculate how much each person will pay
+let individualContribution = grandTotal / totalPeople;
+
+// Print the result to verify the procedure works!
+console.log(individualContribution);
---
+++ codingFoundations/javascriptProject/javascriptBasics/05-booleans.js
@@ -0,0 +1,24 @@
+YES / NO
+ON / OFF
+TRUE / FALSE
+
+let isLoggedIn = true;
+let isAuthenticated = true;
+let hasLoggedInToday = false;
+let hasToken = false;
+
+console.log(2 == '2') // true
+console.log(2 === '2') // false (remember this compares type as well as value!)
+console.log(2 != 3) // true
+console.log(2 !== '2') // true (true since this operator compares type as well)
+console.log(5 > 5) // false
+console.log(5 >= 5) // true
+console.log(7 < 8) //true
+console.log(8 <= 8) //true 
+
+let x = true;
+let y = false;
+
+console.log(x&&y) //false
+console.log(x||y) //true
+console.log(!x) //false  
