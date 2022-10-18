2fdc76b6cf7882a311c2bee64813db302d95ad29
Celio Arias <ariascelio@gmail.com>
2022-10-17 20:13:09 -0400

    Arrays

---
+++ codingFoundations/javascriptProject/javascriptBasics/07-objects.js
@@ -0,0 +1,13 @@
+//1         //2
+let zelda = {
+    //3                 //4
+    name            : "Zelda",
+    age             : 17,
+    title           : "Princess",
+    isCaptured      : true
+};
+console.log(zelda); //1
+console.log(zelda.name); //2
+console.log(zelda.age); //3
+
+console.log(zelda.middleName); //undefined: middleName does not exist
