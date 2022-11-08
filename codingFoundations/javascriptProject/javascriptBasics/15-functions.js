789141fa6dabb6071320e0d314dd5e8aa7437118
Celio Arias <ariascelio@gmail.com>
2022-11-08 01:50:46 -0500

    Pair_Programming

--- codingFoundations/javascriptProject/javascriptBasics/15-functions.js
+++ codingFoundations/javascriptProject/javascriptBasics/15-functions.js
@@ -1,23 +1,3 @@
 function hello() {
     console.log("Hello World!");
 }
-hello(); // invokes the function hello.
-function numberEntered(num){
-    console.log(`The number you entered was: ${num}`);
-}
-function addAnyTwoNumbers(num1, num2){
-    console.log(num1 + num2);
-}
-
-addAnyTwoNumbers(4, 5);
-addAnyTwoNumbers(6, 11);
-addAnyTwoNumbers(7, 8);
-
-//Answer
-//Declare it
-function subtractTwoNumbers(firstNum, secondNum){
-    console.log(firstNum - secondNum);
-}
-
-//Call it
-subtractTwoNumbers(5, 7);
