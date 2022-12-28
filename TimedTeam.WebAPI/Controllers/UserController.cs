fd6123312f52d9c245b4023044c13f3716dca0b6
Caitlin <csimondesign@gmail.com>
2022-12-27 21:06:16 -0500

    Authorizing

--- TimedTeam.WebAPI/Controllers/UserController.cs
+++ TimedTeam.WebAPI/Controllers/UserController.cs
@@ -1,15 +1,70 @@
+using TimedTeam.Models.Token;
+using TimedTeam.Models.User;
+using TimedTeam.Services.Token;
+using TimedTeam.Services.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TimedTeam.WebAPI.Controllers
+namespace Chitter.WebAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly IUserServices _service;
-        public UserController(IUserService service)
+        private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
+        public UserController(IUserService userService, ITokenService tokenService)
         {
-            _service = service;
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("Register User")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registerResult = await _userService.RegisterUserAsync(model);
+
+            if (registerResult)
+            {
+                return Ok("New Chitter user successfully registered!");
+            }
+            return BadRequest("New Chitter user could NOT be created. Please try again.");
+        }
+
+        [Authorize]
+        [HttpGet, Route("{userID}")]
+        public async Task<IActionResult> GetByID(int userID)
+        {
+            var userInfo = await _userService.GetUserByIDAsync(userID);
+
+            if (userInfo is null)
+            {
+                return NotFound();
+            }
+            return Ok(userInfo);
+        }
+
+        [HttpPost, Route("~/api/Token")]
+        public async Task<IActionResult> Token([FromBody] TokenRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var tokenResponse = await _tokenService.GetTokenAsync(request);
+
+            if (tokenResponse is null)
+            {
+                return BadRequest("Invalid username or password.");
+            }
+
+            return Ok(tokenResponse);
         }
     }
 }
