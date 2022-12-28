3b0fe78a2a2b6a78f1c8a6d0315aa92360297eaa
Caitlin <csimondesign@gmail.com>
2022-12-27 19:50:08 -0500

    push

--- TimedTeam.WebAPI/Controllers/UserController.cs
+++ TimedTeam.WebAPI/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TimedTeam.Data;
 
 namespace TimedTeam.WebAPI.Controllers
 {
